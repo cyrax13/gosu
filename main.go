e9c3a33ba8acc2c0b3a948e64cd7e0ccec13d4ea
cyrax13 <bx.mail.bx@gmail.com>
2024-09-07 20:53:18 +0200

    Update golang to 1.22.7

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func init() {
 }
 
 func version() string {
-	// 1.17.4 (go1.22.6 on linux/amd64; gc)
+	// 1.17.4 (go1.22.7 on linux/amd64; gc)
 	return Version + ` (` + runtime.Version() + ` on ` + runtime.GOOS + `/` + runtime.GOARCH + `; ` + runtime.Compiler + `)`
 }
 
