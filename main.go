52e73ea40931f4f98affb018f0f1a43411eb9ff3
cyrax13 <bx.mail.bx@gmail.com>
2024-10-05 02:18:25 +0200

    Update version to 1.17.6

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func init() {
 }
 
 func version() string {
-	// 1.17.5 (go1.22.8 on linux/amd64; gc)
+	// 1.17.6 (go1.22.8 on linux/amd64; gc)
 	return Version + ` (` + runtime.Version() + ` on ` + runtime.GOOS + `/` + runtime.GOARCH + `; ` + runtime.Compiler + `)`
 }
 
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package main
 
-const Version = "1.17.5"
+const Version = "1.17.6"
