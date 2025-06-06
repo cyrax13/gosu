4f3543f991c64b6a2676071339b73e96807afb96
cyrax13 <bx.mail.bx@gmail.com>
2025-06-06 23:17:23 +0200

    Update golang to 1.23.10

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.23.9-bookworm
+FROM golang:1.23.10-bookworm
 
 RUN set -eux; \
 	apt-get update; \
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func init() {
 }
 
 func version() string {
-	// 1.17.13 (go1.23.9 on linux/amd64; gc)
+	// 1.17.13 (go1.23.10 on linux/amd64; gc)
 	return Version + ` (` + runtime.Version() + ` on ` + runtime.GOOS + `/` + runtime.GOARCH + `; ` + runtime.Compiler + `)`
 }
 
